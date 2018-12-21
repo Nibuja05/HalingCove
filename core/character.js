a90d14c8a3e4838c1135be991cbe43d867217e42
aw93mita <aw93mita@studserv.uni-leipzig.de>
2018-12-22 00:42:59 +0100

    add documentation to character.js

--- core/character.js
+++ core/character.js
@@ -1,17 +1,31 @@
 
+/**
+ * creates a new charcter for a user
+ * @param  {int} 		bot       bot identification
+ * @param  {connection} con       database Connection
+ * @param  {int} 		userID    user identification
+ * @param  {int} 		channelID channel identification
+ * @param  {string} 	input     the user command
+ */
 function createNew(bot, con, userID, channelID, input) {
 	console.log("Creating new character...");
 	try {
 		var name = input[0];
+
+		//check if the user entered a name
 		if (name.length == 0) {
 			throw "Not a valid name for the character"
 		}
+
 		var sql = "SELECT cNr FROM charList WHERE name = '" + name + "' AND userID = " + userID + "";
 		con.query(sql, function (err, result) {
 		    if (err) throw err;
+
+		    //check if this character already exists for the user
 			if (result.length == 0) {
 				console.log("New character, named " + name)
-				console.log("Connected!");
+
+				//set all characters to inactive
 				sql = "SELECT cNr, active FROM charList WHERE active = 1 AND userID = " + userID + "";
 				con.query(sql, function (err, result) {
 				    if (err) throw err;
@@ -23,12 +37,15 @@ function createNew(bot, con, userID, channelID, input) {
 						});
 					});
 				});
+
+				//insert the new character and set it to active
 				sql = "INSERT INTO charList (userID, name, level, class, active) VALUES ('" + userID + "', '" + name + "', '1', 'Unexperienced Adventurer', '1')";
 				con.query(sql, function (err, result) {
 				    if (err) throw err;
 					console.log("[DB] 1 record inserted (charList)");
 					show(bot, con, userID, channelID)
 				});
+
 			} else {
 				var text = "You already have a character with the name **" + name + "**!";
 				printMessage(bot, channelID, text);
@@ -47,11 +64,20 @@ function createNew(bot, con, userID, channelID, input) {
 	return true;
 }	
 
+/**
+ * sends a message with information about the current character of this user
+ * @param  {int} 		bot       bot identification
+ * @param  {connection} con       database Connection
+ * @param  {int} 		userID    user identification
+ * @param  {int} 		channelID channel identification
+ */
 function show(bot, con, userID, channelID) {
 	try {
 		var sql = "SELECT userList.name AS userName, charList.name AS characterName, charList.level, charList.class FROM charList INNER JOIN userList ON charList.userID = userList.userID WHERE charList.active = 1 AND userList.userID = " + userID;
 	    con.query(sql, function (err, result) {
 	        if (err) throw err;
+
+	        //check if there is an active character
 	        if (result.length > 0) {
 	        	var uName = result[0].userName;
 		       	var charName = "**" + result[0].characterName + "**";
@@ -59,6 +85,7 @@ function show(bot, con, userID, channelID) {
 		       	var className = result[0].class;
 		       	var text = "<@" + userID + ">: You're currently playing as the " + className + " " + charName + " lvl " + level + ".";
 		       	printMessage(bot, channelID, text); 
+
 	       	} else {
 	       		var text = "You have no active character!"
 	       		printMessage(bot, channelID, text)
@@ -70,19 +97,35 @@ function show(bot, con, userID, channelID) {
 	}
 } 
 
+/**
+ * deletes a character with given name of a user. needs verification
+ * @param  {int} 		bot       bot identification
+ * @param  {connection} con       database Connection
+ * @param  {int} 		userID    user identification
+ * @param  {int} 		channelID channel identification
+ * @param  {string} 	input     the user command
+ * @param  {string} 	confirm   user reaction
+ */
 function deleteChar(bot, con, userID, channelID, input, confirm) {
 	try {
 		var name = input[0];
+
+		//check if the user entered a name
 		if (name.length == 0) {
 			throw "Not a valid name for the character";
 		}
+
 		var found = false;
 		var sql = "SELECT cNr, name FROM charList WHERE name = '" + name +"' AND userID = " + userID + "";
 		con.query(sql, function (err, result) {
 		    if (err) throw err;
+
+		    //test if there is the character exists
 			if (result.length > 0) {
 				found = true;
 			};
+
+			//if the user answered with no, cancel the action
 			if (confirm == "no" || confirm == "n") {
 				sql = "UPDATE lastCommand SET optValues = 'none' WHERE userID = " + userID;
                 con.query(sql, function (err, result) {
@@ -93,6 +136,8 @@ function deleteChar(bot, con, userID, channelID, input, confirm) {
                 printMessage(bot, channelID, text);
 				return;
 			}
+
+			//if the user confirmed the action, continue
 			if (found == true && (confirm == "yes" || confirm == "y")) {
 				console.log("Deleting the character " + name);
 				sql = "DELETE FROM charList WHERE name = '" + name +"' AND userID = " + userID + "";
@@ -107,6 +152,8 @@ function deleteChar(bot, con, userID, channelID, input, confirm) {
                     if (err) throw err;
                     console.log("[DB] 1 record updated (lastCommand)")
                 });
+
+            //if there is no confirmation yet, ask the user if he wants to continue
 			} else if(found == true && confirm == false) {
 				sql = "UPDATE lastCommand SET optValues = 'confirm' WHERE userID = " + userID;
                 con.query(sql, function (err, result) {
@@ -129,22 +176,37 @@ function deleteChar(bot, con, userID, channelID, input, confirm) {
 	return true;
 }
 
+/**
+ * selects a character with given name of the user
+ * @param  {int} 		bot       bot identification
+ * @param  {connection} con       database Connection
+ * @param  {int} 		userID    user identification
+ * @param  {int} 		channelID channel identification
+ * @param  {string} 	input     the user command
+ */
 function select(bot, con, userID, channelID, input) {
 	try {
 		var name = input[0];
 		var sql = "SELECT cNr FROM charList WHERE userID = " + userID + " AND name = '" + name + "'";
 	    con.query(sql, function (err, result) {
 	        if (err) throw err;
+
+	        //check if the character exists
 	        if (result.length > 0) {
 	        	var newChar = result[0];
 	        	console.log(newChar)
-	        	var oldChar = 0;
 	        	sql = "SELECT cNr FROM charList WHERE userID = " + userID + " AND active = 1";
 	        	con.query(sql, function (err, result) {
 				    if (err) throw err;
-				    oldChar = result[0];
+				    var oldChar = result[0];
+
+				    //check if there is an active character
 					if (oldChar != undefined) {
+
+						//check if the character is already active
 						if (oldChar.cNr != newChar.cNr) {
+
+							//set the old character to inactive and the new one to active
 							sql = "UPDATE charList SET active = 0 WHERE cNr = " + oldChar.cNr;
 							con.query(sql, function (err, result) {
 								if (err) throw err;
@@ -154,10 +216,13 @@ function select(bot, con, userID, channelID, input) {
 									show(bot, con, userID, channelID);
 								});
 							});
+
 						} else {
 							var text = "This character is already active"
 	       					printMessage(bot, channelID, text)
 						}
+
+					//set the new character to active
 					} else {
 						sql = "UPDATE charList SET active = 1 WHERE cNr = " + newChar.cNr;
 						con.query(sql, function (err, result) {
@@ -166,6 +231,7 @@ function select(bot, con, userID, channelID, input) {
 						});
 					}
 				});
+
 	       	} else {
 	       		var text = "This character does not exist!"
 	       		printMessage(bot, channelID, text)
@@ -177,10 +243,19 @@ function select(bot, con, userID, channelID, input) {
 	}
 }
 
+/**
+ * sends a message with a list of all characters of a user
+ * @param  {int} 		bot       bot identification
+ * @param  {connection} con       database Connection
+ * @param  {int} 		userID    user identification
+ * @param  {int} 		channelID channel identification
+ */
 function showAll(bot, con, userID, channelID) {
 	var sql = "SELECT name, level, class FROM charList WHERE userID = " + userID + "";
 	con.query(sql, function (err, result) {
 	    if (err) throw err;
+
+	    //iterate over every result and add it to the list, then send the list as message
 	    var text = "Currently you own following characters:\n";
 		result.forEach(function(res) {
 			var row = " - **" + res.name + "**, " + res.class + " level " + res.level + "\n";
@@ -190,6 +265,12 @@ function showAll(bot, con, userID, channelID) {
 	});
 }
 
+/**
+ * sends a message in a channel
+ * @param  {int} 		bot       bot identification
+ * @param  {int} 		channelID channel identification
+ * @param  {string} 	text      the content of the message
+ */
 function printMessage(bot, channelID, text) {
     bot.sendMessage({
         to: channelID,
